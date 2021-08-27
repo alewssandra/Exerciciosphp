5b828c44e230d11c61a2e6da4dc2488dc8903364
Alessandra Lima <56393487+alewssandra@users.noreply.github.com>
2021-08-27 13:55:19 -0300

    Create inicio.php

---
+++ intervalo/inicio.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto 2</title>
+	<meta charset="utf-8">
+
+</head>
+<body>
+	<B><H1>SEJA BEM VINDO</H1></B><br><br>
+<form action="gerador.php" method="post">
+
+<B> Digite o valor inicial</B> <input type="text" name="inicial" required=""> <br><br>
+<B> Digite o valor final</B> <input type="text" name="final" required=""> <br><br><br>
+<input type="submit" name="Enviar ">
+<input type="reset" value="Apagar Dados">
+<br><br>
+</form>
+</body>
+</html>
