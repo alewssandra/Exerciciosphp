3387c46bce270beedf2b3d3d5c109a3264d3bfe2
Alessandra Lima <56393487+alewssandra@users.noreply.github.com>
2021-08-30 14:03:04 -0300

    Create inicio.php

---
+++ salario/inicio.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exer 1 Prova</title>
+	<meta charset="utf-8">
+
+</head>
+<body>
+	<B><H1>Aumento Salarial</H1></B><br><br>
+
+<form action="calcular.php" method="post">
+
+<B> Digite o seu nome</B> <input type="text" name="nome" required=""> <br><br>
+<B> Digite seu salário </B> <input type="text" name="salario" required=""> <br><br>
+<input type="submit" name="Enviar ">
+<input type="reset" value="Apagar Dados">
+<br><br>
+</form>
+</body>
+</html>
